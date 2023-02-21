54016b2cfa54a7a10e0a2d3a2ded01a4ab6dd025
Jetrca92 <blaz.guzej@gmail.com>
2023-02-21 19:34:50 +0100

    Project setup

---
+++ library.js
@@ -0,0 +1,11 @@
+let myLibraty = [];
+
+function Book(title, author, pages, read) {
+    this.title = title;
+    this.author = author;
+    this.pages = pages;
+    this.read = read;
+    this.info = function () {
+        console.log(`${title} by ${author}, ${pages} pages, ${read} yet`);
+    }
+}
